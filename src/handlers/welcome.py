bc7ae473d25703d3e115bf45a611ac043ae78832
sumtopmus <sumtopmus@users.noreply.github.com>
2022-12-01 08:42:01 -0500

    Bugfix: #about is not accepted when added to message on edit.

--- src/handlers/welcome.py
+++ src/handlers/welcome.py
@@ -73,19 +73,23 @@ async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE) -> State:
 async def about(update: Update, context: ContextTypes.DEFAULT_TYPE) -> State:
     """When a user writes #about."""
     tools.debug('about')
-    tools.clear_jobs(context, f'{WELCOME_TIMEOUT_JOB}:{update.message.from_user.id}')
-    context.user_data['about'] = update.message.text
-    message = f'Вітаємо тебе, {tools.mention(update.message.from_user)}!'
-    # TODO: handle the case when update.message.message_thread_id is incorrect.
+    if update.message:
+        incoming_message = update.message
+    else:
+        incoming_message = update.edited_message
+    tools.clear_jobs(context, f'{WELCOME_TIMEOUT_JOB}:{incoming_message.from_user.id}')
+    context.user_data['about'] = incoming_message.text
+    message = f'Вітаємо тебе, {tools.mention(incoming_message.from_user)}!'
+    # TODO: handle the case when incoming_message.message_thread_id is incorrect.
     welcome_message = await context.bot.sendMessage(
-            chat_id=update.message.chat.id, message_thread_id=config.WELCOME_THREAD_ID,
-            text=message, reply_to_message_id=update.message.id)
+            chat_id=incoming_message.chat.id, message_thread_id=config.WELCOME_THREAD_ID,
+            text=message, reply_to_message_id=incoming_message.id)
     context.job_queue.run_once(
             tools.message_cleanup,
             config.WELCOME_CLEANUP_PERIOD,
             data=welcome_message.id,
             name=f'welcome_cleanup',
-            chat_id=update.message.chat.id)
+            chat_id=incoming_message.chat.id)
     return ConversationHandler.END
 
 
