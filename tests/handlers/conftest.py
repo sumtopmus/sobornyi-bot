89717438c1864193d20834ff695b470767efbffc
sumtopmus <sumtopmus@users.noreply.github.com>
2025-03-11 00:49:10 -0400

    Added unit tests for war handlers

--- tests/handlers/conftest.py
+++ tests/handlers/conftest.py
@@ -14,5 +14,6 @@ def patch_settings(mock_settings):
         patch("handlers.request.settings", mock_settings),
         patch("handlers.topic.settings", mock_settings),
         patch("handlers.upload.settings", mock_settings),
+        patch("handlers.war.settings", mock_settings),
     ):
         yield
